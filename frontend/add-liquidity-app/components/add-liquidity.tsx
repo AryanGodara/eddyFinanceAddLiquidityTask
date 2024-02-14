5bf01fbc432765ff6195c699dd3dfc21c3c5743a
Aryan Godara <aryangodara03@gmail.com>
2024-02-14 21:04:33 +0530

    add feature to calculate correct ratio

--- frontend/add-liquidity-app/components/add-liquidity.tsx
+++ frontend/add-liquidity-app/components/add-liquidity.tsx
@@ -5,11 +5,43 @@ import {
     useAccount,
     useWriteContract
 } from 'wagmi' 
-import { abi, contractAddress } from './abi'
+import { abi, contractAddress, pairAbi, pairAddress } from './abi'
+import useDebounce from './useDebounce';
+
 import axios from 'axios';
 import styles from '../styles/Home.module.css';
+import { readContract } from 'viem/actions';
+import { createPublicClient, http } from 'viem'
+import { zetachainAthensTestnet } from 'viem/chains';
 
 export function AddLiquidity() {
+    const [usdcAmount, setUsdcAmount] = React.useState('');
+    const [usdtAmount, setUsdtAmount] = React.useState('');
+    const debouncedUsdcAmount = useDebounce(usdcAmount, 500);
+
+    async function fetchPoolRatio() {
+        const client = createPublicClient({
+            chain: zetachainAthensTestnet,
+            transport: http("https://rpc.ankr.com/zetachain_evm_athens_testnet"),
+          });
+        
+        const res = await readContract(client, {
+            address: pairAddress,
+            abi: pairAbi,
+            functionName: 'getReserves',
+        })
+        return res[0]/res[1]; // This is just a placeholder
+    }
+
+    React.useEffect(() => {
+        if (debouncedUsdcAmount) {
+            fetchPoolRatio().then((ratio) => {
+                const newUsdtAmount = BigInt(debouncedUsdcAmount) * ratio;
+                setUsdtAmount(newUsdtAmount.toString());
+            });
+        }
+    }, [debouncedUsdcAmount]);
+
     const { 
         data: hash,
         error,
@@ -28,10 +60,15 @@ export function AddLiquidity() {
     async function submit(e: React.FormEvent<HTMLFormElement>) { 
         e.preventDefault()
         const formData = new FormData(e.target as HTMLFormElement) 
-        const usdcAmount = formData.get('usdcAmount') as string
-        const usdtAmount = formData.get('usdtAmount') as string
+        const usdcAmount = (formData.get('usdcAmount') as string) + '000000000000000000';
+        const usdtAmount = formData.get('usdtAmount') as string + '000000000000000000';
         const address = account.address ? account.address.substring(2) : '';
-        console.log("SEnding Transaction")
+
+        
+
+        
+
+        console.log("Sending Transaction")
         writeContract({ 
             address: contractAddress, 
             abi, 
@@ -55,31 +92,29 @@ export function AddLiquidity() {
 
     return (
         <form onSubmit={submit} className={styles.form}>
-            {/* <input name="usdcAmount" placeholder="69420" required /> */}
-            {/* <input name="usdtAmount" placeholder="" required /> */}
             <div className={styles.inputContainer}>
+                <b>USDC Token (Enter Manually)</b>
                 <input 
                     className={styles.inputField}
                     name="usdcAmount" 
+                    value={usdcAmount}
+                    onChange={(e) => setUsdcAmount(e.target.value)}
                     placeholder="USDC Amount" 
                     required 
                 />
             </div>
             <div className={styles.inputContainer}>
+            <b>USDC Token (We calculate the ratio for you)</b>
                 <input 
                     className={styles.inputField}
                     name="usdtAmount" 
+                    value={usdtAmount}
+                    onChange={(e) => setUsdtAmount(e.target.value)}
                     placeholder="USDT Amount" 
                     required 
                 />
             </div>
-            {/* <button 
-                disabled={isPending} 
-                type="submit"
-            >
-                {isPending ? 'Confirming...' : 'Add Liquidity'}
-            </button>
-             */}
+
             <button 
                 className={`${styles.button} ${isPending ? styles.buttonDisabled : ''}`}
                 disabled={isPending} 
